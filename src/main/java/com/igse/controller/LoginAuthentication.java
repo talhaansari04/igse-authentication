f96fe8f70f8ead68a2d4d9c07934ad8878b82653
talhaansari04 <talhaansari61@gmail.com>
2024-09-17 21:04:44 +0530

    Added Banner

--- src/main/java/com/igse/controller/LoginAuthentication.java
+++ src/main/java/com/igse/controller/LoginAuthentication.java
@@ -26,15 +26,15 @@ public class LoginAuthentication {
     private final AuthService authService;
 
     @PostMapping(path = "v1/login", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserResponse> loginAuthV1(
+    public final ResponseEntity<UserResponse> loginAuthV1(
             @RequestBody @Validated(value = LoginVersion.LoginV1.class)
-            @JsonView(value = LoginVersion.LoginV1.class) LoginRequest loginRequest) {
+            @JsonView(value = LoginVersion.LoginV1.class) final LoginRequest loginRequest) {
         log.info("Login Authentication Start {}", loginRequest.getCustomerId());
         return ResponseEntity.status(HttpStatus.OK).body(authService.v1Login(loginRequest));
     }
 
     @PostMapping(path = "v2/login", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserResponse> loginAuthV2(
+    public final ResponseEntity<UserResponse> loginAuthV2(
             @RequestBody @Validated(value = LoginVersion.LoginV2.class)
             @JsonView(value = LoginVersion.LoginV2.class) LoginRequest loginRequest) {
         return ResponseEntity.status(HttpStatus.OK).body(authService.v2Login(loginRequest));
