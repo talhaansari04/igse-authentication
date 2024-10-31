23b13125a28a5343a180030b604f18db67fb49d6
talhaansari04 <talhaansari61@gmail.com>
2024-10-31 23:21:10 +0530

    Added Config

---
+++ src/test/java/com/igse/common/StubServer.java
@@ -0,0 +1,9 @@
+package com.igse.common;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+public class StubServer {
+    public static void startStubServer(){
+        WireMockServer mockServer=new WireMockServer();
+    }
+}
--- src/test/java/component/com/igse/test/service/AdminServiceTest.java
+++ src/test/java/component/com/igse/test/service/AdminServiceTest.java
@@ -13,8 +13,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ComponentTest
-@Sql(scripts = { "/init-database.sql", "/populate-data.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(scripts = "/cleanup.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Sql(scripts = { "/sql/cleanup_dashboard_admin.sql","/sql/dashboard_admin.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+@Sql(scripts = "/sql/cleanup_dashboard_admin.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_CLASS)
 class AdminServiceTest {
 
     @Autowired
@@ -22,7 +22,7 @@ class AdminServiceTest {
 
     @Test
     void admin_Dashboard_Success200(){
-        UserResponse userResponse = adminService.dashBoardData("test-123", UUID.randomUUID().toString());
+        UserResponse userResponse = adminService.dashBoardData("test@gmail.com", UUID.randomUUID().toString());
         assertNotNull(userResponse);
     }
 
